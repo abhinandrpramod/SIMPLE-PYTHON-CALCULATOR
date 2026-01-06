df3da83f7c7cf773bebbda27c7b7c03bec7eb7b5
abhinandrpramod <abhinandrpramod@gmail.com>
2026-01-06 19:40:47 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,11 @@
-operator=
+operator = input("enter a operator( + - * / ):")
+num1 = input("enter first number:")
+num2 = input("enter the second number:")
+if operator == "+":
+  result = (num1 + num2)
+ elif operator == "-":
+  result = (num1 - num2)
+ elif operator == "*":
+  result = (num1 * num2)
+ elif operator == "/":
+  result = (num1 / num2)
