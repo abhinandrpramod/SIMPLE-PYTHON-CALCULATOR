2576ab0e8911bd30faf2d3ecbdc82e1b406a5b23
abhinandrpramod <abhinandrpramod@gmail.com>
2026-01-06 21:02:21 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,12 +3,16 @@ num1 = float(input("enter first number:"))
 num2 = float(input("enter the second number:"))
 if operator == "+":
   result = (num1+num2)
+  print("the result is:",result)
 elif operator == "-":
   result = (num1-num2)
+  print("the result is:",result)
 elif operator == "*":
   result = (num1*num2)
+  print("the result is:",result)
 elif operator == "/":
   result = (num1/num2)
+  print("the result is:",result)
 else:
   result = "invalid operator"
-print("the result is:",result)
+print(result)
