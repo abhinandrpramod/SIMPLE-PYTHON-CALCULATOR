2bc526223c8d213fbcf98d9261a3ed2366abf462
abhinandrpramod <abhinandrpramod@gmail.com>
2026-01-13 20:06:19 +0530

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,11 +1,10 @@
 temp=float(input("Enter the temperature:"))
 unit=input("Is the temperature in celsius or fahrenheit (C/F):")
-if unit == "F":
+if unit.upper() == "F":
     temp=((temp-32)*5/9)
     print(f"{round(temp,1)}C")
-elif unit == "C":
+elif unit.upper() == "C":
     temp=((temp*(9/5))+32)
     print(f"{round(temp,1)}F")
 else:
     print(f"{unit} is not valid")
-
