88f9c4bd286113f6cae85de6262ffff12c5faa7c
abhinandrpramod <abhinandrpramod@gmail.com>
2026-01-14 19:11:48 +0530

    Update shoppingcart.py

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,5 +1,19 @@
-foods=[]
+items=[]
 prices=[]
 total=0
-while=True:
-    food=input("enter the food (enter e for exiting)")
+while True:
+    item=input("enter the item (enter e for exiting):")
+    if item.lower()=="e":
+        break
+    else:
+        price=float(input(f"enter the price of {item}:$"))
+        items.append(item)
+        prices.append(price)
+print("------YOUR CART------")
+for item in items:
+    print(item,end=" ")
+for price in prices:
+    total+=price
+
+print()
+print(f"your cart value is:${total}")
