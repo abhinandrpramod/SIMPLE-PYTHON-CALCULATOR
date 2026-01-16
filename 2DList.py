547cfba5abe276ad99500a44d092d80186bf6c1e
abhinandrpramod <abhinandrpramod@gmail.com>
2026-01-16 16:19:36 +0530

    Update 2DList.py

--- 2DList.py
+++ 2DList.py
@@ -1,4 +1,7 @@
 menu=[["chicken","mutton","egg"],
       ["salad","soup","fruits"],
       ["ice cream","juice","shake"]]
-
+for food in menu:
+    for x in food:
+        print(x,end=" ")
+    print()
